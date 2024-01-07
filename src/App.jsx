47bb1ba35087ed66f37db88abcd34d280989aa35
Minnael <minnaelcampelo@gmail.com>
2024-01-07 10:29:55 -0300

    learning flexbox concepts

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,35 @@ import Jogador from './components/Jogador/Jogador.jsx'
 
 import bmwPreta from './assets/bmw-preta.png'
 import ferrari from './assets/ferrari.webp'
+import audi from './assets/audi.jpg'
+
 import lebron from './assets/lebron-james.webp'
+import curry from './assets/curry.jpg'
+import kobe from './assets/kobe.jpg'
 
 function App() {
   return (  
-    <div className='App'>
-      <Carro foto={bmwPreta} cor="ESCURA" marca="BMW"/>
-      <Carro foto={ferrari} cor="AMARELA" marca="FERRARI"/>
-     <Jogador foto={lebron} nome="LEBRON JAMES" esporte="BASQUETE"/>
+    <div className='App'> 
+      <ul className='carros'> 
+        <li>
+          <Carro foto={bmwPreta} cor="ESCURA" marca="BMW"/>
+        </li>
+        <li>  
+          <Carro foto={ferrari} cor="AMARELA" marca="FERRARI"/>
+        </li>
+        <li>
+          <Carro foto={audi} cor="AZUL" marca="AUDI"/>
+        </li>
+        <li>
+          <Jogador foto={lebron} nome="LEBRON JAMES" esporte="BASQUETE"/>
+        </li>
+        <li>
+          <Jogador foto={curry} nome="STEPHEN CURRY" esporte="BASQUETE"/>
+        </li>
+        <li>
+          <Jogador foto={kobe} nome="KOBE BRYANT" esporte="BASQUETE"/>
+        </li>
+      </ul>
     </div>
   )
 }
--- src/components/Carro.jsx
+++ src/components/Carro.jsx
@@ -1,7 +1,7 @@
 function Carro({foto, marca, cor}){
    return(
-      <div className="text-lg font-mono font-black">
-         <img src={foto} alt={marca} className="w-2/6 mx-auto"/>
+      <div>
+         <img src={foto} alt={marca}/>
          <h1>COR: {cor}</h1>
          <h1>MARCA: {marca}</h1>
       </div>
